bfdcdce016896447589d686f327b29893a666c8a
Nikita Chetvertukhin <lardgreat@gmail.com>
2026-05-13 12:51:02 +0200

    fix(test): absolut path for login button

--- locators/login_locators.py
+++ locators/login_locators.py
@@ -3,6 +3,6 @@ from locators.base_locators import BaseLocators
 class LoginLocators(BaseLocators):
     USERNAME_INPUT = '//input[contains(@autocomplete,"username")]'
     PASSWORD_INPUT = '//input[contains(@autocomplete,"password")]'
-    LOGIN_BUTTON = '//a[contains(@class, "btn primary push")]'
+    LOGIN_BUTTON = '//a[@class="btn primary push"]'
 
     AUTH_ERROR = '//div[contains(@class, "error")]//span[text()="Неверный логин или пароль"]'
